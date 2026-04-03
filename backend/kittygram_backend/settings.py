fe375432fc058b7dd3197faee7d1db29917e0fd5
Islam Lurjie <ramzislam125@gmail.com>
2026-04-03 19:29:17 +0300

    the end

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -105,7 +105,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.getenv('MEDIA_ROOT', str(BASE_DIR / 'media'))
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
